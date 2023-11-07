0fe17e093178d47a49ecb6d910a96bfb29e4f56f
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2023-11-07 15:42:10 +0500

    add test display_last_operations2

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,29 +1,27 @@
-import json
-import os
+from src.operations_for_test import operation_list
+from src.utils import display_last_operations, sort_execute
 
-from src.utils import display_last_operations
+
+testing_list = operation_list
 
 
 def test_display_last_operations():
-    # Подготовьте файл "operations.json" с тестовыми данными
-    test_data = {
-        "id": 1,
-        "state": "TEST_STATE",
-        # Другие поля...
-    }
-
-    with open("operations.json", "w", encoding="utf-8") as file:
-        json.dump(test_data, file)
-
-    # Вызов функции и проверка результата
-    result = display_last_operations()
-    assert result == test_data  # Проверьте, что результат соответствует ожидаемым данным
-
-    # Очистите файл после теста (если это необходимо)
-    os.remove("operations.json")
-
-if __name__ == '__main__':
-    pytest.main()
+    assert testing_list == display_last_operations()
 
 
+def test_sort_execute(testing_list):
+    id_list = []
+    state_list = []
+    for one in testing_list:
+        if "id" in one:
+            id_list.append(one["id"])
+        if 'state' in one:
+            if one['state'] == "EXECUTED":
+                state_list.append(one)
+            else:
+                continue
+        else:
+            continue
 
+    assert sort_execute(id_list) == []
+    assert sort_execute(testing_list) == state_list
